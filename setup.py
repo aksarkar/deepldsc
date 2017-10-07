7680948a955b631d445d601e4a9c286e70c145d1
Abhishek Sarkar <aksarkar@alum.mit.edu>
2017-10-06 19:25:59 -0500

    Write up the idea

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='deepldsc',
+    description='Deep LD Score Regression',
+    version='0.1',
+    url='https://www.github.com/aksarkar/deepldsc',
+    author='Abhishek Sarkar',
+    author_email='aksarkar@uchicago.edu',
+    license='MIT',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pandas', 'tensorflow'],
+)
